2d01e461eed80ffae99eb495a50600425a8ed8c0
El-Glory <elgloryoriabure@gmail.com>
2021-06-16 12:36:58 +0100

    created models

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Product = require('./Product.js');
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema {
+	firstName : {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	email: {
+	    type: String,
+	    required: true,
+  },
+  	password:{
+	  	type: String,
+	  	reqired: true,
+  },
+  cart: {
+    items: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    price: {
+      type: Number,
+      default: 0,
+    },
+  }
+}
+
+
+module.exports = mongoose.model("User", userSchema)
